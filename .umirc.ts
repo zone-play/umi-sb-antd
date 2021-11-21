c5ec73580332006811729dbdd4bd3c02a8bc931a
DanaCoding <modana@protonmail.com>
2021-11-21 23:41:45 +0800

    umi配置

--- .umirc.ts
+++ .umirc.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'umi';
+// import { defineConfig } from 'umi';
 
-export default defineConfig({
-  nodeModulesTransform: {
-    type: 'none',
-  },
-  routes: [
-    { path: '/', component: '@/pages/index' },
-  ],
-  fastRefresh: {},
-});
+// export default defineConfig({
+//   nodeModulesTransform: {
+//     type: 'none',
+//   },
+//   routes: [
+//     { path: '/', component: '@/pages/index' },
+//   ],
+//   fastRefresh: {},
+// });
---
+++ config/routes.ts
@@ -0,0 +1,5 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  routes: [{ exact: true, path: '/', component: 'index' }],
+});
