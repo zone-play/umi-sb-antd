c1abd93df504aa2fef89962c2684e4a9cc934b5f
DanaCoding <modana@protonmail.com>
2021-11-21 15:40:19 +0800

    button组件使用图标

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,20 @@
 import styles from './index.less';
 import { ButtonComponent } from '../components/Button';
+import { GithubOutlined } from '@ant-design/icons';
 
 export default function IndexPage() {
-
-const button_type = 'primary';
-const button_label = 'hello world'
+  const button_type = 'primary';
+  const button_label = 'hello world';
 
   return (
     <div>
-      <h1 className={styles.title}>Page index</h1><br />
-      <ButtonComponent type={button_type} label={button_label} />
+      <h1 className={styles.title}>Page index</h1>
+      <br />
+      <ButtonComponent
+        type={button_type}
+        label={button_label}
+        icon={<GithubOutlined />}
+      />
     </div>
   );
 }
