7568337bf1008e30e790fef23fd037ccd445c9cb
DanaCoding <modana@protonmail.com>
2021-11-21 15:36:04 +0800

    更新注释说明

--- src/stories/components/button.stories.tsx
+++ src/stories/components/button.stories.tsx
@@ -4,18 +4,18 @@ import { ButtonComponent } from '../../components/Button';
 import { ComponentStory } from '@storybook/react';
 import { GithubOutlined } from '@ant-design/icons';
 
-// 1. 定义故事
+// 1. 设置故事归属
 export default {
   title: 'Components',
   component: ButtonComponent,
 };
 
-// 2. 获取组件参数
+// 2. 获取组件参数并赋值给模板
 const Template: ComponentStory<typeof ButtonComponent> = (args) => (
   <ButtonComponent {...args} />
 );
 
-// 3. 绑定故事模板
+// 3. 绑定模板并导出故事名称
 export const Button = Template.bind({}); //Button
 
 // 4. 给故事设置默认值
