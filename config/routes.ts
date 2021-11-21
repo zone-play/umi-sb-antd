f0ecd944a1342834d00d8ca33be30928a92029fa
DanaCoding <modana@protonmail.com>
2021-11-21 23:49:46 +0800

    路由使用绝对路径

--- config/routes.ts
+++ config/routes.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'umi';
 
 export default defineConfig({
-  routes: [{ exact: true, path: '/', component: 'index' }],
+  routes: [{ exact: true, path: '/', component: '@/pages/index' }],
 });
