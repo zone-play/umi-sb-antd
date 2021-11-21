1d220bee9eaec49c6982767955760ee606dd292e
DanaCoding <modana@protonmail.com>
2021-11-21 15:16:41 +0800

    故事细节

--- src/stories/components/button.stories.tsx
+++ src/stories/components/button.stories.tsx
@@ -2,17 +2,23 @@ import React from 'react';
 import { ButtonComponent } from '../../components/Button';
 
 import { ComponentStory } from '@storybook/react';
-import {GithubOutlined} from '@ant-design/icons'
+import { GithubOutlined } from '@ant-design/icons';
 
+// 1. 定义故事
 export default {
   title: 'Components',
-  component: ButtonComponent
-} 
+  component: ButtonComponent,
+};
 
-const Template: ComponentStory<typeof ButtonComponent> = (args) => <ButtonComponent {...args} />;
+// 2. 获取组件参数
+const Template: ComponentStory<typeof ButtonComponent> = (args) => (
+  <ButtonComponent {...args} />
+);
 
-export const Button = Template.bind({}) //Button
+// 3. 绑定故事模板
+export const Button = Template.bind({}); //Button
 
+// 4. 给故事设置默认值
 Button.args = {
   type: 'primary', //'primary' | 'ghost' | 'dashed' | 'link' | 'text' | 'default'
   shape: 'round', //'default' | 'circle' | 'round'
@@ -25,6 +31,6 @@ Button.args = {
   icon: null, //ReactNode
   // href: '', //string
   // target: '_blank', //string
-  label: '按钮'
+  label: '按钮',
   // onClick: () => void
-}
+};
