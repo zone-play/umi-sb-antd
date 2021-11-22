6fc4949e0948cd7bfe3a45150eab64a5a50f2a51
DanaCoding <modana@protonmail.com>
2021-11-23 05:43:19 +0800

    logo组件

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { Avatar, Image } from 'antd';
 
 interface LogoProps {
-  alt?: 'TESLA'; //图像无法显示时的替代文本
-  gap?: 4; //4 字符类型距离左右两侧边界单位像素
-  shape?: 'square'; //circle | square
-  size?: { xs: 24; sm: 32; md: 40; lg: 64; xl: 80; xxl: 100 };
-  src?: '../../public/img/logo.jpg';
-  preview?: false;
+  alt?: string; //图像无法显示时的替代文本
+  gap?: number; //4 字符类型距离左右两侧边界单位像素
+  shape?: 'circle' | 'square'; //'circle' | 'square'
+  size?: object;
+  src?: string; // '../../../public/img/logo.jpg';
+  preview?: boolean;
 }
 
 export const LogoComponent = ({
@@ -17,7 +17,6 @@ export const LogoComponent = ({
   size,
   src,
   preview,
-  ...props
 }: LogoProps) => {
   return (
     <Avatar
@@ -26,7 +25,6 @@ export const LogoComponent = ({
       shape={shape}
       size={size}
       src={<Image src={src} preview={preview} />}
-      {...props}
     />
   );
 };
