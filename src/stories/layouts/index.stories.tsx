f4ab96ed78d15fc173e717fe5dbeab0fc397d7d2
DanaCoding <modana@protonmail.com>
2021-11-26 03:55:31 +0800

    layout stories 有点参数

--- src/stories/layouts/index.stories.tsx
+++ src/stories/layouts/index.stories.tsx
@@ -11,17 +11,17 @@ export default {
   component: Layout_Desk_Component,
 };
 
-const Template_Desk: ComponentStory<typeof Layout_Desk_Component> = (args) => (
+const Template_Desk: ComponentStory<typeof Layout_Desk_Component> = () => (
   <Layout_Desk_Component />
 );
 export const LayoutDesk = Template_Desk.bind({});
 
-const Template_Table: ComponentStory<typeof Layout_Table_Component> = (
-  args,
-) => <Layout_Table_Component />;
+const Template_Table: ComponentStory<typeof Layout_Table_Component> = () => (
+  <Layout_Table_Component />
+);
 export const LayoutTable = Template_Table.bind({});
 
-const Template_Mobile: ComponentStory<typeof Layout_Mobile_Component> = (
-  args,
-) => <Layout_Mobile_Component />;
+const Template_Mobile: ComponentStory<typeof Layout_Mobile_Component> = () => (
+  <Layout_Mobile_Component />
+);
 export const LayoutMobile = Template_Mobile.bind({});
